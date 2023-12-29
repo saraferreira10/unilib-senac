9eb57fbf38905a98429e812135e5f5bd1279c707
Sara Ferreira <sara.f.araujo12@gmail.com>
2023-12-29 18:56:57 -0300

    Projeto criado

---
+++ unilib/src/main/java/com/senac/unilib/UnilibApplication.java
@@ -0,0 +1,13 @@
+package com.senac.unilib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnilibApplication.class, args);
+	}
+
+}
---
+++ unilib/src/test/java/com/senac/unilib/UnilibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.unilib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
